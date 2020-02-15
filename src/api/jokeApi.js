dac6e8aa12902dc263a57d2375f4aba4179625e4
Sujan Shrestha <nepkoder@gmail.com>
2020-02-15 13:34:04 +0545

    updating exports for api

--- src/api/jokeApi.js
+++ src/api/jokeApi.js
@@ -1,26 +1,22 @@
-const BASE_URL = 'https://api.chucknorris.io/jokes/';
+export const BASE_URL = 'https://api.chucknorris.io/jokes/';
 
-export default class JokeApi {
+export const getCategories = (endpoint) => {
+     fetch(endpoint).then(res => res.json()).then(result => {
+        return result;
+    }).catch(error => console.log(error));
+}
 
-    static getCategories(endpoint) {
-        fetch(endpoint).then(res => res.json()).then(result => {
-            return result;
-        }).catch(error => console.log(error));
-    }
-
-    static getRandomJoke(category) {
-        let randomCategory = BASE_URL + `random?category=${category}`;
+export const getRandomJoke =(category) => {
+    let randomCategory = BASE_URL + `random?category=${category}`;
         fetch(randomCategory).then(res => res.json()).then(result => {
             return result;
         }).catch(err => err);
-    }
+}
 
-    static getSearchQuery(query) {
-        let searchUrl = BASE_URL + `search?query=${query}`;
-        fetch(searchUrl).then(res => res.json())
-            .then(result => {
-                return result;
-            }).catch(err => err);
-    }
-
+export const getSearchQuery =(query) => {
+    let searchUrl = BASE_URL + `search?query=${query}`;
+    fetch(searchUrl).then(res => res.json())
+        .then(result => {
+            return result;
+        }).catch(err => err);
 }
