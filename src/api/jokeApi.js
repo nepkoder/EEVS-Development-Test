d8f50d3d6eecd1d6ade4a70fd4511d906555f09c
Sujan Shrestha <nepkoder@gmail.com>
2020-02-15 14:09:16 +0545

    default export base url

--- src/api/jokeApi.js
+++ src/api/jokeApi.js
@@ -1,22 +1,22 @@
-export const BASE_URL = 'https://api.chucknorris.io/jokes/';
+export default 'https://api.chucknorris.io/jokes/';
 
-export const getCategories = (endpoint) => {
-     fetch(endpoint).then(res => res.json()).then(result => {
-        return result;
-    }).catch(error => console.log(error));
-}
+// export const getCategories = (endpoint) => {
+//      fetch(BASE_URL + endpoint).then(res => res.json()).then(result => {
+//         return result;
+//     }).catch(error => console.log(error));
+// }
 
-export const getRandomJoke =(category) => {
-    let randomCategory = BASE_URL + `random?category=${category}`;
-        fetch(randomCategory).then(res => res.json()).then(result => {
-            return result;
-        }).catch(err => err);
-}
+// export const getRandomJoke =(category) => {
+//     let randomCategory = BASE_URL + `random?category=${category}`;
+//         fetch(randomCategory).then(res => res.json()).then(result => {
+//             return result;
+//         }).catch(err => err);
+// }
 
-export const getSearchQuery =(query) => {
-    let searchUrl = BASE_URL + `search?query=${query}`;
-    fetch(searchUrl).then(res => res.json())
-        .then(result => {
-            return result;
-        }).catch(err => err);
-}
+// export const getSearchQuery =(query) => {
+//     let searchUrl = BASE_URL + `search?query=${query}`;
+//     fetch(searchUrl).then(res => res.json())
+//         .then(result => {
+//             return result;
+//         }).catch(err => err);
+// }
